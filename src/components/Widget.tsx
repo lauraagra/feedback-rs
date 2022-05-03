c3373bb5c85fcf4f2eb5e517c8e3bc665356100a
Laura Agra <contatolauraagra@gmail.com>
2022-05-03 15:34:37 -0300

    learning and coding application frontend

---
+++ src/components/Widget.tsx
@@ -0,0 +1,21 @@
+import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
+import { Popover } from '@headlessui/react'
+
+export default function Widget() {
+    return (
+        <Popover className="absolute bottom-5 right-5">
+            <Popover.Panel>Hello</Popover.Panel>
+
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+                <ChatTeardropDots className="w-6 h-6" />
+
+                <div className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                    <span className="pl-2">
+                        Feedback
+                    </span>
+                </div>
+            </Popover.Button>
+        </Popover>
+    )
+}
